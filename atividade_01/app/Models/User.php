8d6a52fbf8895fb5ff8f3a1f6e0c77712e556b62
Júlio Leandro <julioleandromds12@gmail.com>
2026-05-11 15:20:51 -0300

    🔧chore: Inclusão de Relacionamento N para N Empréstimo
    - Implementação de um relacionamento N para N entre os modelos User e Book para representar o empréstimo de livros.
    - Criação de uma tabela intermediária chamada borrowings para registrar os detalhes de cada empréstimo.
    - Uso do Tinker para testar Querys avançadas.
    - Teste de comparação Lazy vs Eager.

--- atividade_01/app/Models/User.php
+++ atividade_01/app/Models/User.php
@@ -29,4 +29,12 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function books()
+    {
+        return $this->belongsToMany(Book::class, 'borrowings')
+                    ->withPivot('borrowed_at', 'returned_at')
+                    ->withTimestamps();
+    }
 }
+
